7542eefed94aa9c44030d11710032ee47355d704
John Jairo Restrepo Montoya <johnjairo.restrepo@globant.com>
2022-02-15 16:56:51 -0500

    Topic 3

---
+++ src/Challenge.java
@@ -0,0 +1,23 @@
+import java.text.DecimalFormat;
+
+public class Challenge {
+
+    public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat(".0000");
+        System.out.println(df.format(exp(0.5,10)));
+
+    }
+
+    public static double power(double x, int n) {
+        return (n == 0) ? 1 : x * power(x, n - 1);
+    }
+
+    public static double factorial(int n) {
+        return (n == 0) ? 1 : n * factorial((n - 1));
+    }
+
+    public static double exp(double x, int n) {
+        return (n == 0) ? 1 : (power(x, n) / factorial(n)) + exp(x, n - 1);
+    }
+
+}
