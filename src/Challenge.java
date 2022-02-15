55a3f5bb45a9c5d6cc47c01b6fa3377b3c2c2e9d
John Jairo Restrepo Montoya <johnjairo.restrepo@globant.com>
2022-02-15 17:10:14 -0500

    format

--- src/Challenge.java
+++ src/Challenge.java
@@ -3,8 +3,11 @@ import java.text.DecimalFormat;
 public class Challenge {
 
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat(".0000");
+        DecimalFormat df = new DecimalFormat("0.0000");
+        System.out.println(df.format(exp(4,10)));
+        System.out.println(df.format(exp(20,10)));
         System.out.println(df.format(exp(0.5,10)));
+        System.out.println(df.format(exp(-0.5,10)));
 
     }
 
